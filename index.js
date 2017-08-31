d301408b753cdc024ca0a8b96b71191851a486e4
taddeimania <jtaddei@gmail.com>
2017-08-31 14:34:25 -0400

    Added session management and required login for secret route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const models = require('./models');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -38,17 +39,37 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+app.use(session({
+  secret: 'i am a banana',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const requireLogin = (req, res, next) => {
+  if (req.user !== null) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
 app.get("/", (req, res) => {
   res.render('index');
-})
+});
 
-app.get("/secret", passport.authenticate('local'), (req, res) => {
+app.get("/secret", requireLogin, (req, res) => {
   res.send("SECRET");
 });
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/'
+}));
 
 app.listen(3000)
